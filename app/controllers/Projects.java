724cc78011f9ff1914493818c1f7d2d4ef019bae
Tobias Baum <tbaum@tntinteractive.de>
2015-02-09 14:53:12 +0100

    Export of transcripts only

--- app/controllers/Projects.java
+++ app/controllers/Projects.java
@@ -1,24 +1,26 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import models.Project;
+import models.User;
 
 import org.apache.commons.io.FilenameUtils;
 import org.json.JSONException;
 import org.json.JSONStringer;
 
+import play.mvc.Controller;
+import play.mvc.Result;
+import utils.Export;
+import views.html.projects;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import models.*;
-import play.mvc.*;
-import views.html.*;
-import utils.Export;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 /**
  * The Projects Controller manages all requests from the projects view.
@@ -29,16 +31,17 @@ public class Projects extends Controller {
 	 * / calls this. Only shown if the user is logged in. Otherwise the user
 	 * will be redirect to the login screen. Renders the projects view with all
 	 * projects that the user can access.
-	 * 
+	 *
 	 * @return rendered projects view
 	 * @throws IOException
 	 */
 	public static Result index() throws IOException {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		// only get projects which the current user is authorized to access
-		List<Project> allProjects = Logins.getLoggedInUser().getProjects();
+		final List<Project> allProjects = Logins.getLoggedInUser().getProjects();
 
 		return ok(projects.render("", allProjects));
 	}
@@ -46,23 +49,24 @@ public class Projects extends Controller {
 	/**
 	 * /projects/getInfo calls this. Requires body-data with projectName. All
 	 * Project data as JSON will be returned.
-	 * 
+	 *
 	 * @return Status with JSON of Project datas
 	 */
 	public static Result getInfo() {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		// get Project Id as UUID
-		JsonNode json = request().body().asJson();
-		String projectId = json.findPath("Id").textValue();
+		final JsonNode json = request().body().asJson();
+		final String projectId = json.findPath("Id").textValue();
 
-		UUID uuid = UUID.fromString(projectId);
+		final UUID uuid = UUID.fromString(projectId);
 		if (uuid == null) {
 			return badRequest("Es wurde kein Projekt für diese Aktion gefunden.");
 		}
 
-		Project project = Project.getProject(uuid);
+		final Project project = Project.getProject(uuid);
 		if (project == null) {
 			return badRequest("Das Projekt konnte nicht in der Datenbank gefunden werden.");
 		}
@@ -76,7 +80,7 @@ public class Projects extends Controller {
 		// send informations of the project to the user interface
 		try {
 			return ok(project.toJSON().toString());
-		} catch (JSONException e) {
+		} catch (final JSONException e) {
 			e.printStackTrace();
 
 			// return empty json string
@@ -88,23 +92,24 @@ public class Projects extends Controller {
 	/**
 	 * /projects/deleteProjectrequires calls this. JSON with "Name": name of
 	 * specific Project to be deleted. Deletes the Project.
-	 * 
+	 *
 	 * @return Status with error or success message
 	 */
 	public static Result deleteProject() {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		// get Project Id as UUID
-		JsonNode json = request().body().asJson();
-		String projectId = json.findPath("Id").textValue();
-		UUID uuid = UUID.fromString(projectId);
+		final JsonNode json = request().body().asJson();
+		final String projectId = json.findPath("Id").textValue();
+		final UUID uuid = UUID.fromString(projectId);
 
 		if (uuid == null) {
 			return badRequest("Es wurde kein Projekt für diese Aktion gefunden.");
 		}
 
-		Project project = Project.getProject(uuid);
+		final Project project = Project.getProject(uuid);
 
 		// check if the currently logged in user has the right to delete the
 		// project
@@ -122,12 +127,13 @@ public class Projects extends Controller {
 
 	/**
 	 * /projects/getAllUserNames calls this. Get all registered Users
-	 * 
+	 *
 	 * @return error if no users exists or success and json with all users
 	 */
 	public static Result getAllUserNames() {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		JSONStringer json = new JSONStringer();
 
@@ -136,7 +142,7 @@ public class Projects extends Controller {
 			json = User.toJSON();
 
 			return ok(json.toString());
-		} catch (JSONException e) {
+		} catch (final JSONException e) {
 			e.printStackTrace();
 			// return empty json string
 			return ok((new JSONStringer().toString()));
@@ -147,23 +153,25 @@ public class Projects extends Controller {
 	 * /projects/editProject calls this. requires JSON with "Id": projectId
 	 * "Users": [ {"Value": UserId, "selected": boolean}, ... ] "Properties" :
 	 * [{"Value": PropertyName, ... ] to edit a new Project
-	 * 
+	 *
 	 * @return Status with Success or Error message
 	 */
 	public static Result editProject() {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
-		JsonNode json = request().body().asJson();
+		final JsonNode json = request().body().asJson();
 
-		if (json == null)
+		if (json == null) {
 			return badRequest("Fehler beim Empfangen der Daten.");
+		}
 
-		String projectId = json.findPath("Id").asText();
+		final String projectId = json.findPath("Id").asText();
 
-		UUID uuid = UUID.fromString(projectId);
+		final UUID uuid = UUID.fromString(projectId);
 
-		Project project = Project.getProject(uuid);
+		final Project project = Project.getProject(uuid);
 
 		// check if the currently logged in user has the right to edit the
 		// project
@@ -174,12 +182,12 @@ public class Projects extends Controller {
 		try {
 			setEditedValues(json, project);
 
-		} catch (IOException e) {
+		} catch (final IOException e) {
 
 			e.printStackTrace();
 			return badRequest("Die Projekt-Eigenschaften konnten nicht geändert werden: "
 					+ e.getMessage());
-		} catch (IllegalArgumentException e) {
+		} catch (final IllegalArgumentException e) {
 
 			e.printStackTrace();
 			return badRequest(e.getMessage());
@@ -189,7 +197,7 @@ public class Projects extends Controller {
 		try {
 			return ok(project.toJSON().toString());
 
-		} catch (JSONException e) {
+		} catch (final JSONException e) {
 
 			e.printStackTrace();
 			return ok("Es ist ein Problem beim Ändern des Projekts aufgetreten: "
@@ -199,7 +207,7 @@ public class Projects extends Controller {
 
 	/**
 	 * Helper method to edit a project with the values of the json string
-	 * 
+	 *
 	 * @param json
 	 *            - JsonNode object with the values to set in project
 	 * @param project
@@ -210,14 +218,14 @@ public class Projects extends Controller {
 	private static void setEditedValues(JsonNode json, Project project)
 			throws JsonProcessingException, IOException,
 			IllegalArgumentException {
-		JsonNode node = new ObjectMapper().readTree(json.toString());
+		final JsonNode node = new ObjectMapper().readTree(json.toString());
 
 		// add/remove users from/to the project
 		JsonNode arrNode = node.get("Users");
 
 		if (arrNode.isArray()) {
-			for (JsonNode objNode : arrNode) {
-				int userId = objNode.get("Value").asInt();
+			for (final JsonNode objNode : arrNode) {
+				final int userId = objNode.get("Value").asInt();
 
 				if (objNode.get("Selected").asBoolean()) {
 					project.addUser(User.findById(userId));
@@ -236,17 +244,17 @@ public class Projects extends Controller {
 		// add/remove properties from/to the project
 		arrNode = node.get("Properties");
 		if (arrNode.isArray()) {
-			List<String> sendedProperties = new ArrayList<String>();
+			final List<String> sendedProperties = new ArrayList<String>();
 
-			for (JsonNode objNode : arrNode) {
+			for (final JsonNode objNode : arrNode) {
 				sendedProperties.add(objNode.get("Value").asText());
 			}
 
-			List<String> projectProperties = project
+			final List<String> projectProperties = project
 					.getPropertyTypesAsStrings();
 
 			// copy sendedProperties to use to add new properties later
-			List<String> allSendedProperties = new ArrayList<String>(
+			final List<String> allSendedProperties = new ArrayList<String>(
 					sendedProperties);
 
 			// sendedProperties contains the properties which stay unedited
@@ -256,7 +264,7 @@ public class Projects extends Controller {
 			projectProperties.removeAll(sendedProperties);
 
 			// remove properties
-			for (String propertyName : projectProperties) {
+			for (final String propertyName : projectProperties) {
 				project.removePropertyType(project
 						.getPropertyTypeByName(propertyName));
 			}
@@ -265,13 +273,13 @@ public class Projects extends Controller {
 			allSendedProperties.removeAll(project.getPropertyTypesAsStrings());
 
 			// add new properties
-			for (String propertyName : allSendedProperties) {
+			for (final String propertyName : allSendedProperties) {
 				project.getPropertyTypeByName(propertyName);
 			}
 		}
 
 		// set project name
-		String projectName = node.get("Name").asText();
+		final String projectName = node.get("Name").asText();
 		project.setName(projectName);
 	}
 
@@ -279,40 +287,44 @@ public class Projects extends Controller {
 	 * /projects/add calls this. requires JSON with "Name": projectName "Users":
 	 * [ UserId, UserId, ... ] "Properties" : [Property, Property, ...] to
 	 * create a new Project
-	 * 
+	 *
 	 * @return Status with Success or Error message
 	 */
 	public static Result addProject() {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		// retrieve the asynchronous send JSON data
-		JsonNode json = request().body().asJson();
+		final JsonNode json = request().body().asJson();
 
 		// check whether the body contains data
-		if (json == null)
+		if (json == null) {
 			return badRequest("Es wurden keine Daten übermittelt.");
+		}
 
-		String projectName = json.findPath("Name").asText();
+		final String projectName = json.findPath("Name").asText();
 
 		// check whether the user does not chose a name
-		if (projectName == null || projectName.isEmpty())
+		if (projectName == null || projectName.isEmpty()) {
 			return badRequest("Es wird ein Projektname benötigt.");
+		}
 
 		// check if the name already exists
-		if (Project.getProject(projectName) != null)
+		if (Project.getProject(projectName) != null) {
 			return badRequest("Es ist bereits ein Projekt mit Namen \""
 					+ projectName + "\" vorhanden.");
+		}
 
-		Project newProject = Project.createProject(projectName);
+		final Project newProject = Project.createProject(projectName);
 
 		// try to extract JSON data and set the project values
 		try {
 			setValues(json, newProject);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 			return badRequest(e.getMessage());
-		} catch (IllegalArgumentException e) {
+		} catch (final IllegalArgumentException e) {
 			e.printStackTrace();
 			return badRequest(e.getMessage());
 		}
@@ -321,7 +333,7 @@ public class Projects extends Controller {
 
 		try {
 			result = newProject.toJSON().toString();
-		} catch (JSONException e) {
+		} catch (final JSONException e) {
 			e.printStackTrace();
 			return badRequest("Das Projekt konnte nicht angezeigt werden.");
 		}
@@ -331,7 +343,7 @@ public class Projects extends Controller {
 
 	/**
 	 * Helper method to set the values of a project
-	 * 
+	 *
 	 * @param json
 	 *            - JsonNode object with the values to set in project
 	 * @param project
@@ -342,7 +354,7 @@ public class Projects extends Controller {
 	private static void setValues(JsonNode json, Project newProject)
 			throws JsonProcessingException, IOException,
 			IllegalArgumentException {
-		JsonNode node = new ObjectMapper().readTree(json.toString());
+		final JsonNode node = new ObjectMapper().readTree(json.toString());
 
 		// select JSON node
 		JsonNode arrNode = node.get("Users");
@@ -354,7 +366,7 @@ public class Projects extends Controller {
 
 		// get user IDs and add the respective users to project
 		if (arrNode.isArray()) {
-			for (JsonNode objNode : arrNode) {
+			for (final JsonNode objNode : arrNode) {
 				newProject.addUser(User.findById(objNode.asInt()));
 			}
 		}
@@ -370,7 +382,7 @@ public class Projects extends Controller {
 
 		// get property names and add them to the project
 		if (arrNode.isArray()) {
-			for (JsonNode objNode : arrNode) {
+			for (final JsonNode objNode : arrNode) {
 				System.out.println(objNode.get("Value").asText());
 				newProject.getPropertyTypeByName(objNode.get("Value").asText());
 			}
@@ -381,23 +393,24 @@ public class Projects extends Controller {
 	 * /exportProject/[ID] calls this. Find the project by name and use the
 	 * Export class in package 'utils' to create a ZIP-file and send it directly
 	 * to the end-user.
-	 * 
+	 *
 	 * @param projectName
 	 * @return
 	 * @throws IOException
 	 */
 	public static Result exportProject(String projectId) throws IOException {
-		if (!Logins.isLoggedIn())
+		if (!Logins.isLoggedIn()) {
 			return redirect(routes.Logins.login());
+		}
 
 		// get Project Id as UUID
-		UUID uuid = UUID.fromString(projectId);
+		final UUID uuid = UUID.fromString(projectId);
 		if (uuid == null) {
 			return badRequest("Es konnte kein Projekt zum gegebenen Namen gefunden werden!");
 		}
 
 		// find project by uuid
-		Project project = Project.getProject(uuid);
+		final Project project = Project.getProject(uuid);
 		if (project == null) {
 			return badRequest("Es konnte kein Projekt zum gegebenen Namen gefunden werden!");
 		}
@@ -409,9 +422,52 @@ public class Projects extends Controller {
 		}
 
 		// create ZIP-file
-		File exportedZip = Export.exportProject(project);
+		final File exportedZip = Export.exportProject(project);
+
+		return sendZip(exportedZip);
+	}
+
+
+	/**
+	 * /exportTranscripts/[ID] calls this. Find the project by name and use the
+	 * Export class in package 'utils' to create a ZIP-file and send it directly
+	 * to the end-user.
+	 *
+	 * @param projectName
+	 * @return
+	 * @throws IOException
+	 */
+	public static Result exportTranscripts(String projectId) throws IOException {
+		if (!Logins.isLoggedIn()) {
+			return redirect(routes.Logins.login());
+		}
+
+		// get Project Id as UUID
+		final UUID uuid = UUID.fromString(projectId);
+		if (uuid == null) {
+			return badRequest("Es konnte kein Projekt zum gegebenen Namen gefunden werden!");
+		}
+
+		// find project by uuid
+		final Project project = Project.getProject(uuid);
+		if (project == null) {
+			return badRequest("Es konnte kein Projekt zum gegebenen Namen gefunden werden!");
+		}
+
+		// check if the currently logged in user has the right to export the
+		// project
+		if (!project.findUser(Logins.getLoggedInUser())) {
+			return badRequest("Sie sind nicht berechtigt dieses Projekt zu exportieren.");
+		}
+
+		// create ZIP-file
+		final File exportedZip = Export.exportTranscripts(project);
 
 		// send ZIP-file to the end-user
+		return sendZip(exportedZip);
+	}
+
+	private static Result sendZip(File exportedZip) {
 		response().setContentType("application/zip");
 		response().setHeader(
 				"Content-disposition",
--- app/utils/Export.java
+++ app/utils/Export.java
@@ -1,11 +1,5 @@
 package utils;
 
-import models.Interview;
-import models.Project;
-import models.PropertyType;
-import models.Statement;
-import models.Code;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -15,91 +9,100 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-
-import org.json.JSONException;
-import org.json.JSONWriter;
-
-import org.apache.commons.io.FilenameUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import models.Code;
+import models.Interview;
+import models.Project;
+import models.PropertyType;
+import models.Statement;
+
+import org.apache.commons.io.FilenameUtils;
+import org.json.JSONException;
+import org.json.JSONWriter;
+
 public class Export {
 	private static final String illegalCharacters = "[^A-Za-z0-9,;:._-]";
+
 	/**
 	 * Writes all properties of one Project to a File (using JSON). It goes through all objects that are accumulated by the project, too.
 	 * Finally a ZIP archive is generated, containing the JSON file and all audio files.
-	 * 
+	 *
 	 * @param source The project that should get exported
 	 * @return returns a File object referring to the generated ZIP file
 	 * @throws IOException
 	 */
 	public static File exportProject(Project source) throws IOException{
-		File tmpFolder = File.createTempFile("export_", "");
+		final File tmpFolder = File.createTempFile("export_", "");
 		tmpFolder.delete(); //file was created but we need a folder
 		tmpFolder.mkdir();
-		
-		String name = source.getName().replaceAll(illegalCharacters, "?");
-		File jsonFile = new File(tmpFolder.getAbsolutePath()+"/"+name+".json");
-		File zipFile = new File(tmpFolder.getAbsolutePath()+"/"+name+".zip");
-		
-		List<File> exportFiles = new ArrayList<File>();
+
+		final String name = makeLegalName(source.getName());
+		final File jsonFile = new File(tmpFolder.getAbsolutePath(), name+".json");
+		final File zipFile = new File(tmpFolder.getAbsolutePath(), name+".zip");
+
+		final List<File> exportFiles = new ArrayList<File>();
 		exportFiles.add(jsonFile);
-		
-		Writer w = new FileWriter(jsonFile);
-		JSONWriter json = new JSONWriter(w);
+
+		final Writer w = new FileWriter(jsonFile);
+		final JSONWriter json = new JSONWriter(w);
 		try {
 			json.object()
 				.key("name").value(source.getName());
-			
+
 			json.key("interviews").array();
-			for (Interview i : source.getInterviews()) {
+			for (final Interview i : source.getInterviews()) {
 				json.object();
 				json.key("name").value(i.getName());
-				
+
 				//DynamicProperties
-				for (PropertyType type : source.getPropertyTypes()) {
+				for (final PropertyType type : source.getPropertyTypes()) {
 					json.key(type.getName()).value(i.getProperty(type));
 				}
-				
+
 				//AudioFile
 				if (i.getAudio()!=null) {
-					File audioFile = fetchFile(i.getAudio().getFile(), tmpFolder, i.getAudio().getName());
+					final File audioFile = fetchFile(i.getAudio().getFile(), tmpFolder, i.getAudio().getName());
 					exportFiles.add(audioFile);
 					json.key("audio").value(audioFile.getName());
 				}
-				
+
 				//Statements
 				json.key("statements").array();
-				for (Statement s : i.getStatements()) {
+				for (final Statement s : i.getStatements()) {
 					json.object();
 					json.key("time").value(s.getTime().toString());
 					json.key("description").value(s.getDescription());
 					json.key("codes").array();
-					for (Code c : s.getCodes()) {
+					for (final Code c : s.getCodes()) {
 						json.value(c.getName());
 					}
 					json.endArray();
 					json.endObject();
 				}
 				json.endArray();
-				
+
 				json.endObject();
 			}
 			json.endArray();
-			
+
 			json.endObject();
-		} catch (JSONException e) {
+		} catch (final JSONException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Not able to create JSON for project "+source.getName()+".");
 		}
 		w.close();
-		
+
 		return zip(exportFiles, zipFile);
 	}
-	
+
+	private static String makeLegalName(String s) {
+		return s.replaceAll(illegalCharacters, "?");
+	}
+
 	private static File fetchFile(File origin, File dest, String name) throws IOException{
 		//find new filename
 		String newName = name;
@@ -108,33 +111,71 @@ public class Export {
 			i++;
 			newName = FilenameUtils.getBaseName(origin.getName())+"_"+Integer.toString(i)+"."+FilenameUtils.getExtension(origin.getName());
 		}
-		
+
 		Files.copy(origin.toPath(), Paths.get(dest.getAbsolutePath()+"/"+newName), StandardCopyOption.COPY_ATTRIBUTES);
 		return new File(dest.getAbsolutePath()+"/"+newName);
 	}
-	
+
 	private static File zip(List<File> files, File dest) throws IOException {
 		//zipping all files to zip archive dest
-		FileOutputStream out = new FileOutputStream(dest);
-		ZipOutputStream zipStream = new ZipOutputStream(out);
-		byte[] buffer = new byte[1024];
-		
-		for (File f : files) {
-			FileInputStream in = new FileInputStream(f);
-			ZipEntry entry = new ZipEntry(f.getName());
+		final FileOutputStream out = new FileOutputStream(dest);
+		final ZipOutputStream zipStream = new ZipOutputStream(out);
+		final byte[] buffer = new byte[1024];
+
+		for (final File f : files) {
+			final FileInputStream in = new FileInputStream(f);
+			final ZipEntry entry = new ZipEntry(f.getName());
 			zipStream.putNextEntry(entry);
-			
+
 			int len;
 			while((len=in.read(buffer)) > 0) {
 				zipStream.write(buffer, 0, len);
 			}
-			
+
 			zipStream.closeEntry();
 			in.close();
 		}
-		
+
 		zipStream.close();
 		out.close();
 		return dest;
 	}
+
+	/**
+	 * Exports all transcripts as text combined into a zip file.
+	 *
+	 * @param source The project that should get exported
+	 * @return returns a File object referring to the generated ZIP file
+	 */
+	public static File exportTranscripts(Project source) throws IOException {
+		final File tmpFile = File.createTempFile(makeLegalName(source.getName()) + "_export_", ".zip");
+
+		final ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(tmpFile));
+		try {
+			for (final Interview i : source.getInterviews()) {
+				final ZipEntry entry = new ZipEntry(makeLegalName(i.getName()) + ".txt");
+				zip.putNextEntry(entry);
+				//Byte Order Mark, damit UTF-8 besser erkannt wird
+				zip.write(0xEF);
+				zip.write(0xBB);
+				zip.write(0xBF);
+				for (final Statement s : i.getStatementsSorted()) {
+					zip.write(s.getTime().toString().getBytes("UTF-8"));
+					zip.write('\t');
+					zip.write(s.getDescription().getBytes("UTF-8"));
+					zip.write('\r');
+					zip.write('\n');
+				}
+				zip.closeEntry();
+			}
+		} catch (final JSONException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Not able to export transcripts for project "+source.getName()+".", e);
+		} finally {
+			zip.close();
+		}
+
+		return tmpFile;
+	}
+
 }
